377e6d865ff1c67877e506e3e6a3fdb1fda30e76
katjam <katja@neontribe.co.uk>
2015-11-17 18:19:19 +0000

    Track a link clicks by page as events and all hashChanges

---
+++ src/js/seap-analytics.js
@@ -0,0 +1,31 @@
+// Which c-app site are we?
+var siteTitle = $(document).find("title").text();
+    site = 'c-app';
+    if (siteTitle === 'ESA Assessment Support') {
+      site = 'esa';
+    }
+    if (siteTitle === 'PIP Assessment Support') {
+      site = 'pip';
+    }
+
+var siteAss = site + 'Ass';
+
+// Track all slides as pageviews
+$(window).on('hashchange', function(e) {
+  // @todo add list of # not to track. For now let's call every change a page.
+  var page = window.location.hash;
+  ga('send', 'pageview', page);
+});
+
+// Event on every a
+$('body').on('click', 'a', function(e) {
+  var page = '';
+      linkText = $(this).text();
+  // Attempt to get the page we came from in the hashHistory.
+  // If that fails, call it unknown.
+  page = _.last(window.hashHistory);
+  if (!page) {
+    page = 'unknown';
+  }
+  ga('send', 'event', page, 'a-link-click', linkText, null);
+});

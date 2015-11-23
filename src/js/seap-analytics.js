26d1c48211c8812ca9206b29e3469ca20bbd2c69
katjam <katja@neontribe.co.uk>
2015-11-23 14:16:01 +0000

    Stats analytics tested with PIP

--- src/js/seap-analytics.js
+++ src/js/seap-analytics.js
@@ -42,3 +42,25 @@ $('body').on('click', 'button', function(e) {
   }
   ga('send', 'event', page, 'button-click', linkText, null);
 });
+
+// Event on visit to stats page
+$('#stats-content').on('stats-analytic-event', function(e) {
+  var numAll = window.allQuestions.length;
+      numSeen = db.get(siteAss).seenQuestions.length;
+      numSkipped = db.get(siteAss).skippedQuestions.length;
+      answers = db.get(siteAss).answers;
+      numAnswered = 0;
+
+      $.each(answers, function(key, value) {
+        numAnswered += _.size(value);
+      });
+
+  // Percentage of questions seen and skipped.
+  var percentSkipped = (numSkipped/numSeen).toFixed(2);
+  ga('send', 'event', '#stats', 'question-progress', 'percent skipped', percentSkipped);
+
+  // Percentage of all questions answered (including ones not yet seen).
+  var percentAnswered = (numAnswered/numAll).toFixed(2);
+  ga('send', 'event', '#stats', 'question-progress', 'percent answered', percentAnswered);
+
+});

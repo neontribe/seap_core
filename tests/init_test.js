fc610a1d4c6e21b5ce656a2fce98444dadc18ccb
Nikomus <nick.wade@live.co.uk>
2019-03-19 16:16:19 +0000

    Make better use of existing button-shaped class

--- tests/init_test.js
+++ tests/init_test.js
@@ -9,8 +9,8 @@
       test.assertHttpStatus(200, 'c-App is up');
       test.assertTitle('c-App', 'c-App index page title is the one expected');
       test.assertElementCount('header nav li a', 2, '2 header menu links');
-      test.assertElementCount('.wrapper .esa-section a.button-link', 1, 'ESA link in view');
-      test.assertElementCount('.wrapper .pip-section a.button-link', 1, 'ESA link in view');
+      test.assertElementCount('.wrapper .esa-section a.button-shaped', 1, 'ESA link in view');
+      test.assertElementCount('.wrapper .pip-section a.button-shaped', 1, 'ESA link in view');
       test.assertExists('footer .social-profile-buttons', 'Social buttons in footer');
       test.assertExists('footer .winner', 'Award winner image in footer');
       return test.assertExists('footer div div a', 3, 'footer contains 3 sponsor links');
